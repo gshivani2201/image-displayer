cb0ce1ca537a6bac6973eee504bf6a7f402b8aaf
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-06-10 22:50:11 +0530

    fetching data, search bar, sorting dropdown

--- src/mycomponents/Header.js
+++ src/mycomponents/Header.js
@@ -2,7 +2,29 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Sort by
+                            </a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="#">Ascending order</a></li>
+                                <li><a className="dropdown-item" href="#">Descending order</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
   )
 }
 
--- src/mycomponents/MainBody.js
+++ src/mycomponents/MainBody.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function MainBody() {
+
+    const [images, setImages] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/photos?albumId=1')
+             .then (res => {
+                console.log(res)
+                setImages(res.data)
+             })
+             .catch (err => {
+                 console.log(err)
+             })
+    }, [])
+
   return (
-    <div>MainBody</div>
+    <table>
+        {
+            images.map(image => (
+                <tr>
+                    <td>{image.id}</td>
+                    <td>{image.title}</td>
+                    <td>
+                        <img src={image.thumbnailUrl} alt={image.title} />
+                    </td>
+                </tr>
+            ))
+        }
+    </table>
   )
 }
 
